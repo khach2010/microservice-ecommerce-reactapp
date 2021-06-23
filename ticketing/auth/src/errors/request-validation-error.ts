6addf97ecfe901db084ec0e31162f282864ceeff
tu le <khach2010@gmail.com>
2021-06-23 09:56:05 +0200

    final error related code - how to define a new custom errors

--- ticketing/auth/src/errors/request-validation-error.ts
+++ ticketing/auth/src/errors/request-validation-error.ts
@@ -1,10 +1,11 @@
+import { CustomError } from "./custom-error"
 import { ValidationError } from "express-validator";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
     statusCode = 400
 
     constructor (public errors: ValidationError[]) {
-      super()
+      super('invalid request parameters')
 
       //Only because we are extending a built in class
       Object.setPrototypeOf(this, RequestValidationError.prototype)
--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -5,6 +5,8 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
 
 const app = express();
 app.use(json());
@@ -14,7 +16,11 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+app.all('*', () => {
+  throw new NotFoundError()
+})
 app.use(errorHandler)
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!!!!!..');
