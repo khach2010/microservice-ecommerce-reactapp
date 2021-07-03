80e0e03ab598d93946c9887f24d1e1e4db6ac333
tu le <khach2010@gmail.com>
2021-07-03 08:47:53 +0200

    Comparing password hasing

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,3 +1,4 @@
+import { Password } from './../services/password';
 import mongoose from "mongoose";
 
 //create aan interface that describe the properties 
@@ -26,6 +27,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+})
+
+userSchema.pre('save', async function(done) {
+  if(this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'))
+    this.set('password', hashed)
+  }
+  done()
 })
 
 userSchema.statics.build = (attrs: UserAtrrs) => {
