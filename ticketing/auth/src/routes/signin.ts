a030944a7bf1dbea39bd7ec799ebcf27b0e8a950
tu le <khach2010@gmail.com>
2021-07-08 16:03:53 +0200

    Sign In Logic

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,7 +1,10 @@
 import express, {Request, Response} from 'express';
+import jwt from 'jsonwebtoken';
 import { body } from "express-validator";
+import { User } from '../models/user';
+import { Password } from './../services/password';
 import { validateRequest } from '../middlewares/validate-request';
-
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -16,6 +19,32 @@ router.post('/api/users/signin',
     .withMessage('You must supply a password')
 ],
 validateRequest,
-(req: Request, res: Response) => {});
+async (req: Request, res: Response) => {
+  const {email, password} = req.body
+
+  const existingUser = await User.findOne({email})
+
+  if(!existingUser) {
+    throw new BadRequestError('Invalid credentials')
+  }
+
+  const passwordMatch = await Password.compare(existingUser.password, password)
+  if(!passwordMatch) {
+    throw new BadRequestError('Invalid credentials')
+  }
+      //generate JWT
+      const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+      }, process.env.JWT_KEY!) 
+  
+      //store it on session
+      req.session = {
+        jwt: userJwt
+      } 
+  
+      res.status(200).send(existingUser)
+
+});
 
 export { router as signinRouter };
