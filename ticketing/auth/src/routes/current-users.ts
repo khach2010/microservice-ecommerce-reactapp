81e427be2112bae5f6251c0e4eac255e564d968a
tu le <khach2010@gmail.com>
2021-07-09 16:00:31 +0200

    augmenting Type Definitions

--- ticketing/auth/src/routes/current-users.ts
+++ ticketing/auth/src/routes/current-users.ts
@@ -1,22 +1,25 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import { currentUser } from '../middlewares/current-user'
+
+
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req, res ) => {
+router.get('/api/users/currentuser', currentUser ,(req, res ) => {
+  return res.send({currentUser: req.currentUser || null})
   //if(!req.session || !req.session.jwt)
-  if(!req.session?.jwt) {
-    return res.send({currentUser: null})
-  }
-  try {
-    const payload = jwt.verify(
-      req.session.jwt, 
-      process.env.JWT_KEY!
-    )
-    res.send({currentUser: payload})
-  } catch (err) {
-    res.send({currentUser: null})
-  }
-  
+  // if(!req.session?.jwt) {
+  //   return res.send({currentUser: null})
+  // }
+  // try {
+  //   const payload = jwt.verify(
+  //     req.session.jwt, 
+  //     process.env.JWT_KEY!
+  //   )
+  //   res.send({currentUser: payload})
+  // } catch (err) {
+  //   res.send({currentUser: null})
+  // }
 })
 
 export {router as currentUserRouter}
