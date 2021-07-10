cd51c62089ea237cdc2436167f1a440df2fa8e73
tu le <khach2010@gmail.com>
2021-07-10 07:12:04 +0200

    require Auth for Route Access

--- ticketing/auth/src/routes/current-users.ts
+++ ticketing/auth/src/routes/current-users.ts
@@ -1,11 +1,12 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
+// import jwt from 'jsonwebtoken'
 import { currentUser } from '../middlewares/current-user'
+import { requireAuth } from '../middlewares/reuqire-auth'
 
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', currentUser ,(req, res ) => {
+router.get('/api/users/currentuser', currentUser, requireAuth ,(req, res ) => {
   return res.send({currentUser: req.currentUser || null})
   //if(!req.session || !req.session.jwt)
   // if(!req.session?.jwt) {
